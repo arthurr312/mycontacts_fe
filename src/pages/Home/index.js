eb3ffcc1ad3bd09feb91bf11a0cd1b042cc831a3
Arthur Silva <arthur10.silva312@gmail.com>
2023-03-23 08:31:45 -0300

    first commit

---
+++ src/pages/Home/index.js
@@ -0,0 +1,45 @@
+import { Link } from 'react-router-dom';
+import {
+  Container, Header, ListContainer, Card, InputSearchContainer,
+} from './styles';
+import arrow from '../../assets/images/icons/arrow.svg';
+import edit from '../../assets/images/icons/edit.svg';
+import trash from '../../assets/images/icons/trash.svg';
+
+export default function Home() {
+  return (
+    <Container>
+      <InputSearchContainer>
+        <input type="text" placeholder="Pesquisar contato..." />
+      </InputSearchContainer>
+      <Header>
+        <strong>3 contatos</strong>
+        <Link to="/new">Novo contato</Link>
+      </Header>
+      <ListContainer>
+        <header>
+          <button type="button">
+            <span>Nome</span>
+            <img src={arrow} alt="arrow icon" />
+          </button>
+        </header>
+      </ListContainer>
+      <Card>
+        <div className="info">
+          <div className="contact-name">
+            <strong>Mateus Silva</strong>
+            <small>instagram</small>
+          </div>
+          <span>mateus@devacademy.com</span>
+          <span>(41) 99999-9999</span>
+        </div>
+        <div className="actions">
+          <Link to="/edit/123"><img src={edit} alt="edit icon" /></Link>
+          <button type="button">
+            <img src={trash} alt="delete icon" />
+          </button>
+        </div>
+      </Card>
+    </Container>
+  );
+}
